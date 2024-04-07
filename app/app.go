8f4db6a173376d83a93c48ef70066c5ab49083db
toastsandwich <shreyasxcore2003@gmail.com>
2024-04-07 10:41:10 +0530

    printing routes to main

--- app/app.go
+++ app/app.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"fmt"
+
 	"github.com/toastsandwich/LCP/config"
 	"github.com/toastsandwich/LCP/routes"
 )
@@ -8,5 +10,16 @@ import (
 func Start() {
 	echo := config.Echo
 	routes.Init(echo)
+	fmt.Println("endpoints : ")
+	fmt.Println("[+] Doctor")
+	fmt.Println("/getAllDoctors")
+	fmt.Println("/getDoctorByID/:id")
+	fmt.Println("/addDoctor/")
+	fmt.Println("/deleteDoctor/:id")
+	fmt.Println("[+] Lab Assistant")
+	fmt.Println("/getAllLabAssistants")
+	fmt.Println("getLabAssistantByID/:id")
+	fmt.Println("/addLabAssistant/")
+	fmt.Println("/deleteLabAssistant/:id")
 	echo.Logger.Fatal(echo.Start(":8080"))
 }
