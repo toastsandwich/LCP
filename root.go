27691bae33bdd06829a7e34ceaa20c1a2795c8e0
Shreyas Mali <83450905+toastsandwich@users.noreply.github.com>
2024-04-09 17:20:22 +0000

    rework

--- root.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/toastsandwich/LCP/app"
-
-func main() {
-	app.Start(":8081")
-}
--- static/home/home.go
+++
@@ -1,7 +0,0 @@
-package home
-
-import "github.com/labstack/echo/v4"
-
-func Home(ctx echo.Context) error {
-	return ctx.File("static/home/index.html")
-}
