b32b157b69ee0b24b16db7911d7d11f6bd18aa82
toastsandwich <shreyasxcore2003@gmail.com>
2024-10-25 08:58:07 +0530

    Updated handlers

--- cmd/handler/handler.go
+++ cmd/handler/handler.go
@@ -1,11 +1,25 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/toastsandwich/LCP/util"
-	"github.com/toastsandwich/LCP/view/layout"
+	"github.com/toastsandwich/LCP/view/pages"
 )
 
-func Test(ctx echo.Context) error {
-	return util.Render(ctx, 200, layout.Base("test"))
+type Handler struct{}
+
+func Home(c echo.Context) error {
+	return util.Render(c, http.StatusOK, pages.Home())
+}
+
+func Login(c echo.Context) error {
+	return util.Render(c, http.StatusOK, pages.Login())
+}
+
+func LoginPost(c echo.Context) error {
+	email := c.FormValue("emp-id")
+	password := c.FormValue("password")
+	return c.JSON(http.StatusOK, map[string]string{"email": email, "password": password})
 }
--- cmd/web/web.go
+++ cmd/web/web.go
@@ -1,15 +1,23 @@
 package web
 
 import (
-	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/toastsandwich/LCP/cmd/handler"
 )
 
 func Start() error {
 	e := echo.New()
-	e.Use(middleware.Logger())
-	e.GET("/", handler.Test)
 
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.RateLimiter(middleware.NewRateLimiterMemoryStore(20)))
+
+	e.Static("/assets", "assets")
+
+	e.GET("/", handler.Home)
+	e.GET("/login", handler.Login)
+
+	e.POST("/login", handler.LoginPost)
 	return e.Start(":15001")
 }
