9ea9374d69afa0b230211e19ea74db444ccddaa9
toastsandwich <shreyasxcore2003@gmail.com>
2024-10-25 08:58:52 +0530

    Updated build.sh

--- build.sh
+++ build.sh
@@ -6,15 +6,23 @@ set -e
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
+CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
 echo -e "${YELLOW}Building....${NC}"
 
 mkdir -p bin
 
+if templ generate; then
+    echo -e "${GREEN}Templ generatation complete.${NC}"
+else
+    echo -e "${RUN}Templ generatation failed.${NC}"
+    exit 1
+fi
+
 if go build -o bin/server main.go; then
     echo -e "${GREEN}Build complete!${NC}"
-    echo -e "${YELLOW}Check the bin directory.${NC}"
+    echo -e "${CYAN}Check the bin directory.${NC}"
 else
     echo -e "${RED}Build failed!${NC}"
     exit 1
